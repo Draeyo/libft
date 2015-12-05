2c9b64b634bd3fc2d278c2d41d1eef9fc90dcb2b
Victor LISTRAT <vlistrat@e2r5p9.42.fr>
2015-12-05 16:08:24 +0100

    modif

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: vlistrat <vlistrat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/01 18:05:55 by vlistrat          #+#    #+#             */
-/*   Updated: 2015/12/05 14:34:53 by vlistrat         ###   ########.fr       */
+/*   Updated: 2015/12/05 16:08:02 by vlistrat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,17 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list		*new;
 	t_list		**new_list;
-	int			i;
 	t_list		*prev;
 
 	new = NULL;
 	new_list = &new;
-	i = 0;
 	prev = lst;
 	while (lst)
 	{
-		lst = lst->next;
 		prev->next = lst;
-		i++;
+		lst = lst->next;
 	}
+	prev->next = lst;
 	new = ft_lstnew(f(lst)->content, f(lst)->content_size);
 	lst = prev->next;
 	while (lst)
@@ -36,7 +34,6 @@ t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 		new = ft_lstnew(f(lst)->content, f(lst)->content_size);
 		ft_lstadd(new_list, new);
 		lst = prev->next;
-		i--;
 	}
 	return (*new_list);
 }
